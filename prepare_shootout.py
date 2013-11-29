b44cfdc9819d08a8cc862c86b1061689f1155322
Radim Řehůřek <radimrehurek@seznam.cz>
2013-11-29 11:53:40 +0100

    initial commit

---
+++ prepare_shootout.py
@@ -0,0 +1,143 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+# Copyright (C) 2013 Radim Rehurek <radimrehurek@seznam.cz>
+
+"""
+USAGE: %(program)s WIKI_DUMP.xml.bz2 OUTPUT_DIRECTORY
+
+Parse articles from a Wikipedia dump, storing several files ito OUTPUT_DIRECTORY:
+
+* title_tokens.txt.gz: raw article titles and tokens, one article per line, "title[TAB]space_separated_tokens[NEWLINE]"
+* dictionary: pickled dictioanry object containing the word<=>word_id mapping
+* dictionary.txt: same as `dictionary` but in plain text format
+* tfidf.model: pickled TF-IDF model
+* lsi.model: pickled model for latent semantic analysis model, trained on TF-IDF'ed wiki dump
+* lsi_vectors.mm: wikipedia articles stored as vectors in LSI space, in MatrixMarket format
+
+Example:
+    ./prepare_shootout.py ~/data/wiki/enwiki-latest-pages-articles.xml.bz2 ~/data/wiki/shootout
+"""
+
+import logging
+import os
+import sys
+import multiprocessing
+import bz2
+
+import gensim
+
+logger = logging.getLogger('prepare_shootout')
+
+PROCESSES = max(1, multiprocessing.cpu_count() - 1)  # parallelize parsing using this many processes
+
+MIN_WORDS = 50  # ignore articles with fewer tokens (redirects, stubs etc)
+
+NUM_TOPICS = 500  # number of latent factors for LSA
+
+
+def process_article((title, text)):
+    """Parse a wikipedia article, returning its content as `(title, list of tokens)`, all utf8."""
+    text = gensim.corpora.wikicorpus.filter_wiki(text)  # remove markup etc, get plain text
+    return title.encode('utf8'), gensim.utils.simple_preprocess(text)  # tokenize
+
+
+def convert_wiki(infile):
+    """
+    Yield articles from a bz2 Wikipedia dump `infile` as (title, tokens) 2-tuple.
+
+    Only articles of sufficient length are returned (short articles & redirects
+    etc are ignored).
+
+    """
+    logger.info("extracting articles from %s using %i processes" % (infile, PROCESSES))
+    articles, articles_all = 0, 0
+    positions, positions_all = 0, 0
+
+    pool = multiprocessing.Pool(PROCESSES)
+    # process the corpus in smaller chunks of docs, because multiprocessing.Pool
+    # is dumb and would try to load the entire input into RAM...
+    texts = gensim.corpora.wikicorpus._extract_pages(bz2.BZ2File(infile))  # generator
+    for group in gensim.utils.chunkize(texts, chunksize=10 * PROCESSES):
+        for title, tokens in pool.imap(process_article, group):
+            if articles_all % 100000 == 0:
+                logger.info("PROGRESS: at article #%i; accepted %i articles with %i total tokens" %
+                    (articles_all, articles, positions))
+            articles_all += 1
+            positions_all += len(tokens)
+            if len(tokens) >= MIN_WORDS and not title.startswith('Wikipedia:'): # article redirects and short stubs are pruned here
+                articles += 1
+                positions += len(tokens)
+                yield title, tokens
+    pool.terminate()
+
+    logger.info("finished iterating over Wikipedia corpus of %i documents with %i positions"
+        " (total %i articles, %i positions before pruning articles shorter than %i words)" %
+        (articles, positions, articles_all, positions_all, MIN_WORDS))
+
+
+class ShootoutCorpus(gensim.corpora.TextCorpus):
+    def get_texts(self):
+        length = 0
+        lines = gensim.corpora.textcorpus.getstream(self.input)  # open file/reset stream to its start
+        for lineno, line in enumerate(lines):
+            length += 1
+            if length > 2500000:
+                break
+            yield line.split('\t')[1].split()
+        self.length = length
+
+
+if __name__ == '__main__':
+    logging.basicConfig(format='%(asctime)s : %(levelname)s : %(message)s')
+    logging.root.setLevel(level=logging.INFO)
+    logger.info("running %s" % ' '.join(sys.argv))
+
+    # check and process input arguments
+    program = os.path.basename(sys.argv[0])
+    if len(sys.argv) < 3:
+        print globals()['__doc__'] % locals()
+        sys.exit(1)
+    infile, outdir = sys.argv[1:3]
+    outfile = lambda fname: os.path.join(outdir, fname)
+
+    preprocessed_file = outfile('title_tokens.txt.gz')
+    if not os.path.exists(preprocessed_file):
+        with gensim.utils.smart_open(preprocessed_file, 'wb') as fout:
+            for docno, (title, tokens) in enumerate(convert_wiki(infile)):
+                try:
+                    line = "%s\t%s" % (title.replace('\t', ''), ' '.join(tokens))
+                    unicode(line, 'utf8')  # make sure everything's convertible to utf8
+                    fout.write("%s\n" % line)
+                except:
+                    logger.info("invalid line at title %s" % title)
+
+    dict_file = outfile('dictionary')
+    if os.path.exists(dict_file):
+        corpus = ShootoutCorpus()
+        corpus.input = gensim.utils.smart_open(preprocessed_file)
+        corpus.dictionary = gensim.corpora.Dictionary.load(dict_file)
+    else:
+        corpus = ShootoutCorpus(gensim.utils.smart_open(preprocessed_file))
+        corpus.dictionary.filter_extremes(no_below=20, no_above=0.1, keep_n=50000)
+        corpus.dictionary.save(dict_file)
+        corpus.dictionary.save_as_text(dict_file + '.txt')
+
+    tfidf_file = outfile('tfidf.model')
+    if os.path.exists(tfidf_file):
+        tfidf = gensim.models.TfidfModel.load(tfidf_file)
+    else:
+        tfidf = gensim.models.TfidfModel(corpus)
+        tfidf.save(tfidf_file)
+
+    lsi_file = outfile('lsi.model')
+    if os.path.exists(lsi_file):
+        lsi = gensim.models.LsiModel.load(lsi_file)
+    else:
+        lsi = gensim.models.LsiModel(tfidf[corpus], id2word=corpus.dictionary, num_topics=NUM_TOPICS, chunksize=10000)
+        lsi.save(lsi_file)
+
+    vectors_file = os.path.join(outdir, 'lsi_vectors.mm')
+    gensim.corpora.MmCorpus.serialize(vectors_file, (gensim.matutils.unitvec(vec) for vec in lsi[tfidf[corpus]]))
+
+    logger.info("finished running %s" % program)
---
+++ shootout.py
@@ -0,0 +1,186 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+# Copyright (C) 2013 Radim Rehurek <radimrehurek@seznam.cz>
+
+"""
+USAGE: %(program)s INPUT_DIRECTORY
+
+Compare speed and accuracy of several similarity retrieval methods, using the corpus prepared by prepare_shootout.py.
+
+Example: ./shootout.py ~/data/wiki/shootout
+
+"""
+
+import os
+import sys
+import time
+import logging
+import itertools
+import random
+from functools import wraps
+
+import numpy
+
+import gensim
+import pyflann
+import annoy
+
+MAX_DOCS = 1000000  # clip the dataset to be indexed at this many docs, if larger
+NUM_QUERIES = 100
+TOP_N = 50  # how many similars to ask for
+
+logger = logging.getLogger('shootout')
+
+def profile(fn):
+    @wraps(fn)
+    def with_profiling(*args, **kwargs):
+        times = []
+        logger.info("benchmarking %s" % fn.__name__)
+        for _ in xrange(3):  # try running it three times, report the best time
+            start = time.time()
+            ret = fn(*args, **kwargs)
+            times.append(time.time() - start)
+        logger.info("%s took %.3fs" % (fn.__name__, min(times)))
+        logger.debug("%s raw timings: %s" % (fn.__name__, times))
+        return ret
+
+    return with_profiling
+
+@profile
+def gensim_1by1(index, queries):
+    for query in queries:
+        _ = index[query]
+
+@profile
+def gensim_at_once(index, queries):
+    _ = index[queries]
+
+@profile
+def flann_1by1(index, queries):
+    for query in queries:
+        _ = index.nn_index(query, TOP_N)
+
+@profile
+def flann_at_once(index, queries):
+    _ = index.nn_index(queries, TOP_N)
+
+@profile
+def annoy_1by1(index, queries):
+    for query in queries:
+        _ = index.get_nns_by_vector(list(query.astype(float)), TOP_N)
+
+
+def flann_precision(index_gensim, index_flann, queries):
+    correct, diffs = 0, []
+    for query in queries:
+        expected_ids, expected_sims = zip(*index_gensim[query])
+        predicted = index_flann.nn_index(query, TOP_N)
+        correct += len(set(expected_ids).intersection(predicted[0][0]))  # how many top-n did flann get right?
+        predicted_sims = 1 - predicted[1][0] / 2  # convert flann output to cossim score
+        diffs.extend(-predicted_sims + expected_sims)  # how far was flann from the correct values?
+    logger.info("flann precision=%.3f, avg diff=%.3f" %
+        (1.0 * correct / (TOP_N * len(queries)), 1.0 * sum(diffs) / len(diffs)))
+
+def annoy_precision(index_gensim, index_annoy, queries):
+    correct, diffs = 0, []
+    for query in queries:
+        expected_ids, expected_sims = zip(*index_gensim[query])
+        predicted = index_annoy.get_nns_by_vector(list(query.astype(float)), TOP_N)
+        correct += len(set(expected_ids).intersection(predicted))  # how many top-n did annoyy get right?
+        predicted_sims = [numpy.dot(index_gensim.vector_by_id(i), query) for i in predicted]
+        diffs.extend(-numpy.array(predicted_sims) + expected_sims)  # how far was flann from the correct values?
+    logger.info("annoy precision=%.3f, avg diff=%.3f" %
+        (1.0 * correct / (TOP_N * len(queries)), 1.0 * sum(diffs) / len(diffs)))
+
+
+def gensim_precision(index_gensim, queries):
+    correct, diffs = 0, []
+    for query in queries:
+        expected_ids, expected_sims = zip(*index_gensim[query])
+        predicted, predicted_sims = expected_ids, expected_sims
+        correct += len(set(expected_ids).intersection(predicted))  # how many top-n did annoyy get right?
+        diffs.extend(-numpy.array(predicted_sims) + expected_sims)  # how far was flann from the correct values?
+    logger.info("gensim precision=%.3f, avg diff=%.3f" %
+        (1.0 * correct / (TOP_N * len(queries)), 1.0 * sum(diffs) / len(diffs)))
+
+
+if __name__ == '__main__':
+    logging.basicConfig(format='%(asctime)s : %(levelname)s : %(message)s')
+    logging.root.setLevel(level=logging.INFO)
+    logger.info("running %s" % ' '.join(sys.argv))
+
+    # check and process input arguments
+    program = os.path.basename(sys.argv[0])
+    if len(sys.argv) < 2:
+        print globals()['__doc__'] % locals()
+        sys.exit(1)
+    indir = sys.argv[1]
+    lsi_vectors = os.path.join(indir, 'lsi_vectors.mm.gz')
+    sim_prefix = os.path.join(indir, 'index%s' % MAX_DOCS)
+
+    mm = gensim.corpora.MmCorpus(gensim.utils.smart_open(lsi_vectors))
+    num_features = mm.num_terms
+    if os.path.exists(sim_prefix + "_clipped.npy"):
+        logger.info("loading dense corpus")
+        clipped = numpy.load(sim_prefix + "_clipped.npy", mmap_mode='r')
+    else:
+        # precompute the entire corpus as a dense matrix in RAM -- FLANN needs this anyway
+        logger.info("creating dense corpus for FLANN")
+        clipped = gensim.matutils.corpus2dense(itertools.islice(mm, MAX_DOCS), num_features).astype(numpy.float32, order='C').T
+        numpy.save(sim_prefix + "_clipped.npy", clipped)
+    clipped_corpus = gensim.matutils.Dense2Corpus(clipped, documents_columns=False)  # same as the islice(MAX_DOCS) above
+
+    logger.info("selecting %s random documents, to act as top-%s queries" % (NUM_QUERIES, TOP_N))
+    query_docs = random.sample(range(clipped.shape[0]), NUM_QUERIES)
+    queries = clipped[query_docs]
+
+    if os.path.exists(sim_prefix + "_gensim"):
+        logger.info("loading gensim index")
+        index_gensim = gensim.similarities.Similarity.load(sim_prefix + "_gensim")
+        index_gensim.num_best = TOP_N
+    else:
+        logger.info("building gensim index")
+        index_gensim = gensim.similarities.Similarity(sim_prefix, clipped_corpus, num_best=TOP_N, num_features=num_features, shardsize=100000)
+        index_gensim.save(sim_prefix + "_gensim")
+        logger.info("built gensim index %s" % index_gensim)
+
+    gensim_precision(index_gensim, queries)  # sanity check & prewarm mmap memory
+    gensim_1by1(index_gensim, queries)
+    gensim_at_once(index_gensim, queries)
+
+    if 'flann' in program:
+        pyflann.set_distance_type('euclidean')
+        index_flann = pyflann.FLANN()
+        if os.path.exists(sim_prefix + "_flann"):
+            logger.info("loading flann index")
+            index_flann.load_index(sim_prefix + "_flann", clipped)
+        else:
+            logger.info("building FLANN index")
+            # flann; expects index vectors as a 2d numpy array, features = columns
+            params = index_flann.build_index(clipped, algorithm="autotuned", target_precision=0.95, log_level="info")
+            index_flann.save_index(sim_prefix + "_flann")
+            logger.info("built FLANN index %s" % params)
+
+        flann_precision(index_gensim, index_flann, queries)
+        flann_1by1(index_flann, queries)
+        flann_at_once(index_flann, queries)
+
+    if 'annoy' in program:
+        index_annoy = annoy.AnnoyIndex(num_features, metric='euclidean')
+        if os.path.exists(sim_prefix + "_annoy"):
+            logger.info("loading annoy index")
+            index_annoy.load(sim_prefix + "_annoy")
+        else:
+            logger.info("building annoy index")
+            # annoy; expects index vectors as lists of Python floats
+            for i, vec in enumerate(clipped_corpus):
+                index_annoy.add_item(i, list(gensim.matutils.sparse2full(vec, num_features).astype(float)))
+            index_annoy.build(50)
+            index_annoy.save(sim_prefix + "_annoy")
+            logger.info("built annoy index")
+
+        annoy_precision(index_gensim, index_annoy, queries)
+        annoy_1by1(index_annoy, queries)
+
+    logger.info("finished running %s" % program)
